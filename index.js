ed6f31c2df6279a395c0a91ea57b9cac81b72bce
ericamonica <erikalaurindo07@gmail.com>
2021-05-26 11:35:27 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = "Érica"
+let idade = 33 
+let anodenascimento = 1988 
+console.log ("meu ano de nascimento é " + anodenascimento )
+let souhumana = true;
+let a = 12;
+let b = 20;
+console.log (a + b)
+   
